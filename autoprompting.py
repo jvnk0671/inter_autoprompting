c6c13ce480290295b701e346aafbe20c36b37656
Pavel Egorov <pavlik.egoroff@gmail.com>
2026-04-30 23:54:36 +0300

    added coolprompt methods, main pipeline

---
+++ autoprompting.py
@@ -0,0 +1,147 @@
+import logging
+from typing import Any, Optional
+from transformers import AutoTokenizer
+
+from functools import lru_cache
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from cool_prompt import coolprompt_optimize
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class OptimizationResult:
+    optimized_prompt: str
+    init_metric: Optional[Any] = None
+    final_metric: Optional[Any] = None
+    init_tokens: Optional[int] = None
+    final_tokens: Optional[int] = None
+
+
+class PromptOptimizer(ABC):
+    @abstractmethod
+    def optimize(self, prompt: str, ch_lim: int) -> OptimizationResult:
+        pass
+
+
+
+
+class CoolPromptOptimizer(PromptOptimizer):
+    def __init__(self, target_model: str, system_model: str):
+        self.target_model = target_model
+        self.system_model = system_model
+
+    def optimize(self, prompt: str, ch_lim: int) -> OptimizationResult:
+        optimized = coolprompt_optimize(
+            prompt=prompt,
+            model=self.target_model,
+            system_model=self.system_model,
+            ch_lim=ch_lim
+        )
+
+        return OptimizationResult(
+            optimized_prompt=optimized['optimized_prompt'],
+            init_metric=optimized['init_metric'],
+            final_metric=optimized['final_metric']
+        )
+
+
+
+
+class ExampleOptimiser(PromptOptimizer):
+    def optimize(self, prompt: str, ch_lim: int) -> OptimizationResult:
+        return OptimizationResult(optimized_prompt=prompt[:ch_lim])
+
+
+
+
+# TODO: когда окончательно определимся с моделями, можно попробовать скачать файлы токенизаторов
+@lru_cache(maxsize=4)
+def get_tokenizer(model: str):
+    """Получить токенизатор по модели"""
+    logger.info(f"Getting tokenizer...")
+    return AutoTokenizer.from_pretrained(model)
+
+def token_counter(prompt: str, model: str) -> int:
+    """Используя токенизатор, посчитать токены по промпту"""
+    tokenizer = get_tokenizer(model)
+    return len(tokenizer.encode(prompt))
+
+def radical_cut(prompt: str, ch_limit: int, uncertainty: int) -> str:
+    """Прямая обрезка промпта со слегка щадящей погрешностью и приоритетом обрезания символов"""
+    max_limit = uncertainty + ch_limit
+    if len(prompt) <= max_limit:
+        return prompt
+
+    min_limit = max(0, ch_limit - uncertainty)
+    cut = prompt[:max_limit]
+
+    markers_prior = [
+        ['\n'],
+        ['.', '!', '?'],
+        [',', ';'],
+        [' ']
+    ]
+
+    for i in markers_prior:
+        further_idx = max(cut.rfind(t) for t in i)
+        if further_idx >= min_limit:
+            return cut[:further_idx + 1].rstrip(' ')
+
+    space = cut.rfind(' ')
+    if space != -1:
+        return cut[:space]
+
+    return cut
+
+
+
+class Pipeline:
+    def __init__(self, optimizer: PromptOptimizer, model: str):
+        self.optimizer = optimizer
+        self.model = model
+
+    def run(self, prompt: str, ch_limit: int, uncertainty: int) -> OptimizationResult:
+        logger.info(f"Prompt to optimize: {prompt}")
+        res = self.optimizer.optimize(prompt, ch_limit)
+        res.optimized_prompt = radical_cut(res.optimized_prompt, ch_limit, uncertainty)
+        res.init_tokens = token_counter(prompt, self.model)
+        res.final_tokens = token_counter(res.optimized_prompt, self.model)
+
+        logger.info(f"Optimized successfully! {res.init_tokens} -> {res.final_tokens}")
+
+        if res.init_metric is not None:
+            logger.info(f"Additional metrics: {res.init_metric} -> {res.final_metric}")
+
+        return res
+
+
+
+
+if __name__ == '__main__':
+    prompt_test = 'I have a question for you that is directly related to mathematics. Please answer, what is 2 plus 2? Thank you'
+    ch_lim_test = 40
+    unsertainty_test = 35
+    SYSTEM_MODEL = 'meta-llama/llama-3.3-70b-instruct:free'
+    TARGET_MODEL = 'nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free'
+
+    coolprompt_opt = CoolPromptOptimizer(
+        target_model=TARGET_MODEL,
+        system_model=SYSTEM_MODEL
+    )
+
+    pipeline = Pipeline(
+        optimizer=coolprompt_opt,
+        model=SYSTEM_MODEL.replace(':free', '')
+    )
+
+    try:
+        res = pipeline.run(prompt=prompt_test, ch_limit=ch_lim_test, uncertainty=unsertainty_test)
+        logger.info(f"Finally: {res.optimized_prompt}")
+    except Exception as e:
+        logger.error(f"ERROR: \n\n{e}\n")
+
+
---
+++ cool_prompt.py
@@ -0,0 +1,49 @@
+import faulthandler
+faulthandler.enable()
+import os
+from typing import Any
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from coolprompt.assistant import PromptTuner
+
+load_dotenv()
+KEY = os.getenv("OPENROUTER_API_KEY")
+
+# системную модель лучше делать быстродумающей, т.к. существует лимит, который вызывает ошибку
+std_sys_model = 'meta-llama/llama-3.3-70b-instruct:free'
+# с reasoning работает очень долго. наверное, можно сделать как опцию в будущем
+reasoning_trg_model = 'nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free'
+
+
+# изначально планировалось использовать distill, но он делает большое количество запросов, что мешает завершить
+# обработку даже для одного промпта (используем только бесплатные LLM для openrouter, и не хватает лимите)
+def coolprompt_optimize(prompt: str, model:str, ch_lim: int, method: str = 'hype',
+                        system_model: str = std_sys_model) -> dict[str, str | Any]:
+
+    system_llm = ChatOpenAI(openai_api_key=KEY, openai_api_base='https://openrouter.ai/api/v1',model_name=system_model)
+    trg_llm = ChatOpenAI(openai_api_key=KEY, openai_api_base='https://openrouter.ai/api/v1', model_name=model)
+
+    tuner = PromptTuner(target_model=trg_llm, system_model=system_llm)
+    optimized = tuner.run(
+        start_prompt=prompt, 
+        method=method,
+        feedback=False,
+        verbose=2,
+        train_as_test=True,
+        problem_description=f'Strict limitation: the final prompt must not exceed {ch_lim} characters. '
+                            f'Loss of meaning is unacceptable.',
+
+        # по умолчанию, их 10. можно попробовать реализовать параметр "эффективность", который будет влиять на аргумент
+        generate_num_samples=3
+    )
+    return {'optimized_prompt': optimized, 'init_metric': tuner.init_metric, 'final_metric': tuner.final_metric}
+
+
+if __name__ == '__main__':
+    testpr = 'I have a question for you that is directly related to mathematics. Please answer, what is 2 plus 2? Thank you'
+    test_chars_lim = 40
+    res = coolprompt_optimize(testpr, model=std_sys_model, ch_lim=test_chars_lim)
+    for i in res:
+        print(f'{i}\t{res[i]}')
+
